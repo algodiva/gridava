5dfe44ad1c37c3f3100ebb878ebe488cd5f565f3
Ava <44328206+algodiva@users.noreply.github.com>
2024-11-15 15:37:13 -0600

    Ran rustfmt.

--- src/core/grid.rs
+++ src/core/grid.rs
@@ -1,23 +1,22 @@
-use std::error::Error;
 use super::*;
+use std::error::Error;
 
 #[derive(Debug)]
 pub enum GridError {
-	AccessError,
-
+    AccessError,
 }
 
 impl std::fmt::Display for GridError {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		match self {
-			GridError::AccessError => write!(f, "Could not access the collection"),
-			_ => todo!()
-		}
-	}
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            GridError::AccessError => write!(f, "Could not access the collection"),
+            _ => todo!(),
+        }
+    }
 }
 
 impl Error for GridError {}
 
 pub trait Grid<TileType: Tile> {
-	fn get_collection<T: TileCollection<TileType>>(&self) -> Result<T, GridError>;
+    fn get_collection<T: TileCollection<TileType>>(&self) -> Result<T, GridError>;
 }
