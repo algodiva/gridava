2a869e9d206e2421601c4fc80fb3d51c2933f4a0
Ava <44328206+algodiva@users.noreply.github.com>
2024-11-22 01:04:13 -0600

    Preparation for implementing resize functionality. Defined a struct HexTile to store pointer to data alongside the coordinates. Refactored existing functions to facilitate this new data type.

--- src/hex/shape.rs
+++ src/hex/shape.rs
@@ -1,21 +1,43 @@
-use std::ops::AddAssign;
+use std::rc::Rc;
 
 use crate::core::transform::{Transform, Transformable};
 
 use super::coordinate::Axial;
 
+#[derive(Clone, PartialEq, Debug, Default)]
+pub struct HexTile<T> {
+    pub coord: Axial,
+    pub data: Option<Rc<T>>,
+}
+
+impl<T: Default> HexTile<T> {
+    pub fn new(coord: Axial, data: Option<T>) -> HexTile<T> {
+        HexTile { coord, data: data.map(|value| Rc::new(value))}
+    }
+}
+
+impl<T: Default> From<Axial> for HexTile<T> {
+    fn from(value: Axial) -> Self {
+        HexTile::new(value, None)
+    }
+}
+
 // A shape consists of coordinate vectors that define which hexes in relation to the origin are a part of the shape
-#[derive(Clone, PartialOrd, PartialEq, Debug, Default)]
-pub struct Shape<T: Copy + AddAssign + Transformable<T>> {
-    tiles: Vec<T>, // Vector of hex vectors pointing to origin that comprise this shape.
-    pub transform: Transform<T>, // The transformation matrix to convert from parent grid to local space.
+#[derive(Clone, PartialEq, Debug, Default)]
+pub struct HexShape<T> {
+    tiles: Vec<HexTile<T>>, // Vector of hex vectors pointing to origin that comprise this shape.
+    pub transform: Transform<Axial>, // The transformation matrix to convert from parent grid to local space.
 }
 
 // TODO
 //      - scale shape? This will be a complicated task as to interpolate inbetween hexes
 
-impl Shape<Axial> {
-    pub fn new(hex_array: Option<Vec<Axial>>, transform: Option<Transform<Axial>>) -> Self {
+impl<T> HexShape<T>
+{
+    pub fn new(hex_array: Option<Vec<HexTile<T>>>, transform: Option<Transform<Axial>>) -> Self
+    where
+        T: Default
+    {
         Self {
             tiles: hex_array.unwrap_or_default(),
             transform: transform.unwrap_or_default(),
@@ -51,16 +73,30 @@ impl Shape<Axial> {
         }
     }
 
+    pub fn resize(&mut self, scale: (f64, f64)) -> &Self {
+        todo!()
+    }
+
+    // Bake the transform into localspace
+    pub fn bake_transform(&mut self) -> HexShape<T> {
+        todo!()
+    }
+
     // Retrieves the hex array in local space
-    pub fn get_hexes(&self) -> Vec<Axial> {
-        self.tiles.clone()
+    pub fn get_hexes(&self) -> &[HexTile<T>] {
+        self.tiles.as_slice()
     }
 
     // Retrieves the hex array in grid space
-    pub fn get_hexes_gs(&self) -> Vec<Axial> {
+    // Because we modify the HexTile coordinates into gs we must
+    // return a cloned vector instead of a slice.
+    pub fn get_hexes_gs(&mut self) -> Vec<HexTile<T>>
+    where
+        T: Clone
+    {
         self.tiles
             .iter()
-            .map(|&coord| coord.apply_all(self.transform))
+            .map(|tile| {tile.coord.apply_all(self.transform); tile.clone()})
             .collect()
     }
 }
@@ -114,11 +150,14 @@ pub mod shape_builders {
             }
         }
 
-        pub fn solve(&self) -> Vec<Axial> {
+        pub fn solve<T, D>(&self, data: Option<D>, constuctor: &dyn Fn(Axial, Option<D>) -> T) -> Vec<T>
+        where
+            D: Clone
+        {
             let mut vec = vec![];
             for q in self.q_min..=self.q_max {
                 for r in self.r_min.max(-q - self.s_max)..=self.r_max.min(-q - self.s_min) {
-                    vec.push(axial!(q, r));
+                    vec.push(constuctor(axial!(q, r), data.clone()));
                 }
             }
             vec
@@ -130,49 +169,80 @@ pub mod shape_builders {
     // The algorithm *WILL* calculate it's inequalities on EVERY point in the array. So, in example, if you have a point
     // inside a shape, that point will still be calculated but will not change anything about the resultant inequality.
     // points: Coordinates to calculate the system of linear inequalities from.
-    pub fn make_shape(points: &[Axial], origin: Option<Axial>) -> Shape<Axial> {
+    pub fn make_shape<T>(points: &[Axial], origin: Option<Axial>, data: Option<T>, constuctor: &dyn Fn(Axial, Option<T>) -> HexTile<T>) -> HexShape<T>
+    where
+        T: Clone + Default
+    {
         if points.is_empty() {
-            return Shape::new(None, None);
+            return HexShape::new(None, None);
         }
+
         let transform = transform!(origin.unwrap_or(points[0]));
 
-        let hexes = Inequality::new(points).solve();
+        let hexes = Inequality::new(points).solve::<HexTile<T>, T>(data, constuctor);
 
-        Shape::new(Some(hexes), Some(transform))
+        HexShape::new(Some(hexes), Some(transform))
     }
+
+    pub struct ShapeArgs {
+        pub origin: Axial,
+        pub size: u32,
+        pub rot_dir: i32,
+    }
+
+    #[macro_export]
+    macro_rules! shapeargs {
+        ($o:expr, $s:expr, $rd:expr) => {
+            ShapeArgs {
+                origin: $o,
+                size: $s,
+                rot_dir: $rd,
+            }
+        };
+    }
+    pub use shapeargs;
 
     // Creates a line.
     // The shape has hexes originating from (0,0) in local space.
     // origin: Denotes the grid space location the shape occupies. Affects the transform.
-    pub fn line(origin: Axial, size: u32, rot_dir: i32) -> Shape<Axial> {
+    pub fn line<T>(args: ShapeArgs, data: Option<T>, constuctor: &dyn Fn(Axial, Option<T>) -> HexTile<T>) -> HexShape<T> 
+    where
+        T: Clone + Default
+    {
         // Working in local space
         let vertex_a = axial!(0, 0);
-        let vertex_b = vertex_a.make_vector(size as i32, rot_dir);
-        make_shape(&[vertex_a, vertex_b], Some(origin))
+        let vertex_b = vertex_a.make_vector(args.size as i32, args.rot_dir);
+        make_shape(&[vertex_a, vertex_b], Some(args.origin), data, constuctor)
     }
 
     // Creates a regular triangle.
     // The shape has hexes originating from (0,0) in local space.
     // origin: Denotes the grid space location the shape occupies. Affects the transform.
-    pub fn triangle(origin: Axial, size: u32, rot_dir: i32) -> Shape<Axial> {
+    pub fn triangle<T>(args: ShapeArgs, data: Option<T>, constuctor: &dyn Fn(Axial, Option<T>) -> HexTile<T>) -> HexShape<T> 
+    where
+        T: Clone + Default
+    {
         // Working in local space
         let vertex_a = axial!(0, 0);
-        let vertex_b = vertex_a.make_vector(size as i32, rot_dir);
-        let vertex_c = vertex_a.make_vector(size as i32, rot_dir + 1);
+        let vertex_b = vertex_a.make_vector(args.size as i32, args.rot_dir);
+        let vertex_c = vertex_a.make_vector(args.size as i32, args.rot_dir + 1);
 
-        make_shape(&[vertex_a, vertex_b, vertex_c], Some(origin))
+        make_shape(&[vertex_a, vertex_b, vertex_c], Some(args.origin), data, constuctor)
     }
 
     // Creates a regular rhombus.
     // The shape has hexes originating from (0,0) in local space.
     // origin: Denotes the grid space location the shape occupies. Affects the transform.
-    pub fn rhombus(origin: Axial, size: u32, rot_dir: i32) -> Shape<Axial> {
+    pub fn rhombus<T>(args: ShapeArgs, data: Option<T>, constuctor: &dyn Fn(Axial, Option<T>) -> HexTile<T>) -> HexShape<T>
+    where
+        T: Clone + Default
+    {
         // Working in local space
         let vertex_a = axial!(0, 0);
-        let vertex_b = vertex_a.make_vector(size as i32, rot_dir);
-        let vertex_c = vertex_a.make_vector(size as i32, rot_dir + 1);
-        let vertex_d = vertex_b.make_vector(size as i32, rot_dir + 1);
+        let vertex_b = vertex_a.make_vector(args.size as i32, args.rot_dir);
+        let vertex_c = vertex_a.make_vector(args.size as i32, args.rot_dir + 1);
+        let vertex_d = vertex_b.make_vector(args.size as i32, args.rot_dir + 1);
 
-        make_shape(&[vertex_a, vertex_b, vertex_c, vertex_d], Some(origin))
+        make_shape(&[vertex_a, vertex_b, vertex_c, vertex_d], Some(args.origin), data, constuctor)
     }
 }
