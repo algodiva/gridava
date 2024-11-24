58400bd034aad6201d8562e475cd00213aa26b7f
Ava <44328206+algodiva@users.noreply.github.com>
2024-11-23 21:25:26 -0600

    Adding CI support (#16)
    Created main yaml file to ensure PRs into main pass all tests, have no warnings and errors.
    
    To ensure tests pass some warnings were fixed.
    
    Added rustfmt.toml to establish repo fmt requirements.
    Added gitattributes. With this and rustfmt.toml, ensure that the repo is LF and working directory is machine native.

--- src/core/grid.rs
+++ src/core/grid.rs
@@ -1,5 +1,4 @@
-use super::*;
-use std::{collections::HashMap, error::Error};
+use std::error::Error;
 
 #[derive(Debug)]
 pub enum GridError {
@@ -10,7 +9,6 @@ impl std::fmt::Display for GridError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             GridError::AccessError => write!(f, "Could not access the collection"),
-            _ => todo!(),
         }
     }
 }
