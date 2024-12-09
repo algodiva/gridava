3a2c1c6140a52a693ee3f3898ed3c3f1bc631457
S <sevriarch@gmail.com>
2024-12-09 10:40:31 -0500

    now operates on a grid

--- src/hex/render.rs
+++ src/hex/render.rs
@@ -1,72 +1,66 @@
 // SVG file generation for hex grids
 
-use std::collections::HashMap;
-
 use svg::Document;
 use svg::node::element::{Path,SVG,Text};
 use svg::node::element::path::Data;
 
 use crate::hex::grid::{HexGrid,HexOrientation};
-use crate::hex::coordinate::Axial;
+use crate::core::tile::Tile;
 
-// Constant for now, longer-term should be configurable
-const BASE_SHORT: f64 = 26.0;
 #[allow(clippy::excessive_precision)]
-const BASE_LONG: f64 = BASE_SHORT * 1.732050807568877293527446341505872367_f64;
+const SQRT3: f64 = 1.732050807568877293527446341505872367_f64;
+// Constant for now, longer-term should be configurable
 const PAD: f64 = 10.0;
 
-pub fn render_shape<T: Clone>(tiles: HashMap<Axial, T>, orientation: HexOrientation) -> SVG {
-    let grid = HexGrid::<(), ()> {
-        orientation: orientation.clone(),
-        hex_size: (BASE_SHORT * 2.0) as f32,
-        ..HexGrid::default()
-    };
+pub fn render_shape<T: Clone>(grid: HexGrid<i32, Tile<T>>) -> SVG {
+    let size_short = grid.hex_size as f64 * 0.5;
+    let size_long = size_short * SQRT3;
 
     let mut doc = Document::new();
-    let mut max_q = BASE_LONG;
+    let mut max_q = size_long;
     let mut min_q = -max_q;
-    let mut max_r = BASE_SHORT * 2.0;
+    let mut max_r = size_short * 2.0;
     let mut min_r = -max_r;
 
     // For now, tile is unused
-    for (coords, _tile) in tiles.iter() {
+    for (coords, _tile) in grid.tiles.iter() {
         let (base_q, base_r) = grid.hex_to_world(*coords);
         let data = Data::new();
 
         // These only apply for PointyTop
-        if base_q - BASE_LONG < min_q { min_q = base_q - BASE_LONG; }
-        if base_q + BASE_LONG > max_q { max_q = base_q + BASE_LONG; }
+        if base_q - size_long < min_q { min_q = base_q - size_long; }
+        if base_q + size_long > max_q { max_q = base_q + size_long; }
 
-        if base_r - BASE_SHORT * 2.0 < min_r { min_r = base_r - BASE_SHORT * 2.0; }
-        if base_r + BASE_SHORT * 2.0 > max_r { max_r = base_r + BASE_SHORT * 2.0; }
+        if base_r - size_short * 2.0 < min_r { min_r = base_r - size_short * 2.0; }
+        if base_r + size_short * 2.0 > max_r { max_r = base_r + size_short * 2.0; }
 
-        let hexagon = match orientation {
+        let hexagon = match grid.orientation {
             HexOrientation::PointyTop => data
-                .move_to((base_q, base_r + BASE_SHORT * 2.0))
-                .line_to((base_q + BASE_LONG, base_r + BASE_SHORT))
-                .line_to((base_q + BASE_LONG, base_r - BASE_SHORT))
-                .line_to((base_q, base_r - BASE_SHORT * 2.0))
-                .line_to((base_q - BASE_LONG, base_r - BASE_SHORT))
-                .line_to((base_q - BASE_LONG, base_r + BASE_SHORT))
-                .line_to((base_q, base_r + BASE_SHORT * 2.0)),
+                .move_to((base_q, base_r + size_short * 2.0))
+                .line_to((base_q + size_long, base_r + size_short))
+                .line_to((base_q + size_long, base_r - size_short))
+                .line_to((base_q, base_r - size_short * 2.0))
+                .line_to((base_q - size_long, base_r - size_short))
+                .line_to((base_q - size_long, base_r + size_short))
+                .line_to((base_q, base_r + size_short * 2.0)),
             HexOrientation::FlatTop => data
-                .move_to((base_q + BASE_SHORT * 2.0, base_r))
-                .line_to((base_q + BASE_SHORT, base_r + BASE_LONG))
-                .line_to((base_q - BASE_SHORT, base_r + BASE_LONG))
-                .line_to((base_q - BASE_SHORT * 2.0, base_r))
-                .line_to((base_q - BASE_SHORT, base_r - BASE_LONG))
-                .line_to((base_q + BASE_SHORT, base_r - BASE_LONG))
-                .line_to((base_q + BASE_SHORT * 2.0, base_r)),
+                .move_to((base_q + size_short * 2.0, base_r))
+                .line_to((base_q + size_short, base_r + size_long))
+                .line_to((base_q - size_short, base_r + size_long))
+                .line_to((base_q - size_short * 2.0, base_r))
+                .line_to((base_q - size_short, base_r - size_long))
+                .line_to((base_q + size_short, base_r - size_long))
+                .line_to((base_q + size_short * 2.0, base_r)),
         };
 
         let path = Path::new()
             .set("fill", "none")
             .set("stroke", "black")
-            .set("stroke-width", 3)
+            .set("stroke-width", 2)
             .set("d", hexagon);
 
         let txt = format!("{},{}", coords.q, coords.r);
-        let text = Text::new(txt).set("x", base_q).set("y", base_r).set("text-anchor", "middle").set("font-size", 12);
+        let text = Text::new(txt).set("x", base_q).set("y", base_r + 4.0).set("text-anchor", "middle").set("font-size", 12);
 
         doc = doc.clone().add(path).add(text);
     }
@@ -114,7 +108,7 @@ mod tests {
 
         grid.apply_shape(&shape);
 
-        let ret = save_image("test.svg", render_shape::<i32>(grid.tiles, HexOrientation::PointyTop));
+        let ret = save_image("test.svg", render_shape::<i32>(grid));
 
         assert!(ret.is_ok());
     }
