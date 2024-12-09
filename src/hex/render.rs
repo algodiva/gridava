8cc28462656fb6c44cfc3324a8897805d1edd231
S <sevriarch@gmail.com>
2024-12-09 11:54:46 -0500

    correct padding for flat top case

--- src/hex/render.rs
+++ src/hex/render.rs
@@ -12,7 +12,18 @@ const SQRT3: f64 = 1.732050807568877293527446341505872367_f64;
 // Constant for now, longer-term should be configurable
 const PAD: f64 = 10.0;
 
-pub fn render_shape<T: Clone>(grid: HexGrid<i32, Tile<T>>) -> SVG {
+
+/// Create a SVG object containing a rendering of this grid.
+///
+/// # Example
+/// ```
+/// /// ...
+/// use gridava::hex::grid::render;
+///
+/// let my_grid = HexGrid::<i32, ()>::default();
+/// let svg = render_svg(my_grid);
+/// ```
+pub fn render_svg<T: Clone>(grid: HexGrid<i32, Tile<T>>) -> SVG {
     let size_short = grid.hex_size as f64 * 0.5;
     let size_long = size_short * SQRT3;
 
@@ -25,39 +36,46 @@ pub fn render_shape<T: Clone>(grid: HexGrid<i32, Tile<T>>) -> SVG {
     // For now, tile is unused
     for (coords, _tile) in grid.tiles.iter() {
         let (base_q, base_r) = grid.hex_to_world(*coords);
-        let data = Data::new();
+        let mut data = Data::new();
 
-        // These only apply for PointyTop
-        if base_q - size_long < min_q { min_q = base_q - size_long; }
-        if base_q + size_long > max_q { max_q = base_q + size_long; }
+        // Do these apply for FlatTop as well as PointyTop?
+        if grid.orientation == HexOrientation::PointyTop {
+            if base_q - size_long < min_q { min_q = base_q - size_long; }
+            if base_q + size_long > max_q { max_q = base_q + size_long; }
 
-        if base_r - size_short * 2.0 < min_r { min_r = base_r - size_short * 2.0; }
-        if base_r + size_short * 2.0 > max_r { max_r = base_r + size_short * 2.0; }
+            if base_r - size_short * 2.0 < min_r { min_r = base_r - size_short * 2.0; }
+            if base_r + size_short * 2.0 > max_r { max_r = base_r + size_short * 2.0; }
 
-        let hexagon = match grid.orientation {
-            HexOrientation::PointyTop => data
+            data = data
                 .move_to((base_q, base_r + size_short * 2.0))
                 .line_to((base_q + size_long, base_r + size_short))
                 .line_to((base_q + size_long, base_r - size_short))
                 .line_to((base_q, base_r - size_short * 2.0))
                 .line_to((base_q - size_long, base_r - size_short))
                 .line_to((base_q - size_long, base_r + size_short))
-                .line_to((base_q, base_r + size_short * 2.0)),
-            HexOrientation::FlatTop => data
+                .line_to((base_q, base_r + size_short * 2.0));
+        } else {
+            if base_q - size_short * 2.0 < min_q { min_q = base_q - size_short * 2.0; }
+            if base_q + size_short * 2.0 > max_q { max_q = base_q + size_short * 2.0; }
+
+            if base_r - size_long < min_r { min_r = base_r - size_long; }
+            if base_r + size_long > max_r { max_r = base_r + size_long; }
+
+            data = data
                 .move_to((base_q + size_short * 2.0, base_r))
                 .line_to((base_q + size_short, base_r + size_long))
                 .line_to((base_q - size_short, base_r + size_long))
                 .line_to((base_q - size_short * 2.0, base_r))
                 .line_to((base_q - size_short, base_r - size_long))
                 .line_to((base_q + size_short, base_r - size_long))
-                .line_to((base_q + size_short * 2.0, base_r)),
-        };
+                .line_to((base_q + size_short * 2.0, base_r));
+        }
 
         let path = Path::new()
             .set("fill", "none")
             .set("stroke", "black")
             .set("stroke-width", 2)
-            .set("d", hexagon);
+            .set("d", data);
 
         let txt = format!("{},{}", coords.q, coords.r);
         let text = Text::new(txt).set("x", base_q).set("y", base_r + 4.0).set("text-anchor", "middle").set("font-size", 12);
@@ -91,7 +109,18 @@ pub fn render_shape<T: Clone>(grid: HexGrid<i32, Tile<T>>) -> SVG {
         .set("style", "background-color: #DDDDDD; stroke-width: 1px")
 }
 
-pub fn save_image(path: &str, document: SVG) -> Result<(), std::io::Error> {
+/// Save an SVG rendering in a file.
+///
+/// # Example
+/// ```
+/// /// ...
+/// use gridava::hex::grid::render;
+///
+/// let my_grid = HexGrid::<i32, ()>::default();
+/// let svg = render_svg(my_grid);
+/// save_svg("save.svg", svg);
+/// ```
+pub fn save_svg(path: &str, document: SVG) -> Result<(), std::io::Error> {
     svg::save(path, &document)
 }
 
@@ -100,15 +129,31 @@ mod tests {
     use super::*;
     use crate::core::tile::Tile;
     use crate::hex::shape::HexShape;
+    use crate::hex::grid::{HexGrid,HexOrientation};
 
     #[test]
-    fn test_render() {
-        let shape = HexShape::make_rhombus(3, 0, true, || 1);
-        let mut grid = HexGrid::<i32, Tile<i32>>::default();
+    fn test_render_pointy_top() {
+        let shape = HexShape::make_rhombus(2, 0, true, || 1);
+        let mut grid = HexGrid::<i32, Tile<i32>>{ orientation: HexOrientation::PointyTop, ..HexGrid::default() };
 
         grid.apply_shape(&shape);
 
-        let ret = save_image("test.svg", render_shape::<i32>(grid));
+        // TODO: figure out how to test the output for correctness as the result is nondeterministic
+        let svg = render_svg::<i32>(grid);
+        let ret = save_svg("test.svg", svg);
+
+        assert!(ret.is_ok());
+    }
+
+    #[test]
+    fn test_render_flat_top() {
+        let shape = HexShape::make_rhombus(3, 0, true, || 1);
+        let mut grid = HexGrid::<i32, Tile<i32>>{ orientation: HexOrientation::FlatTop, hex_size: 100.0, ..HexGrid::default() };
+
+        grid.apply_shape(&shape);
+
+        let svg = render_svg::<i32>(grid);
+        let ret = save_svg("test.svg", svg);
 
         assert!(ret.is_ok());
     }
